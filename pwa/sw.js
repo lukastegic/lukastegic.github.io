c28124a33bb05630edcf086526c1749326208592
lukastegic <lukastegic95@gmail.com>
2022-03-19 12:15:38 +0100

    Update

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
 let cacheName = "my-first-pwa";
-let filesToCache = ["/", "lukastegic.github.io/pwa/", "css/style.css", "js/main.js"];
+let filesToCache = ["/", "index.html", "css/style.css", "js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", (e) => {
