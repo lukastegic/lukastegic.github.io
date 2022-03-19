20143222104f3e5cf34926a68b93e44b2da9f935
lukastegic <lukastegic95@gmail.com>
2022-03-19 11:57:31 +0100

    ab

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
 let cacheName = "my-first-pwa";
-let filesToCache = ["/", "index.html", "css/style.css", "js/main.js"];
+let filesToCache = ["/", "pwa/index.html", "css/style.css", "js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", (e) => {
